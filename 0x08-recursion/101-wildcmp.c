25f3e5063f2caf8bf9bdd17faa0695d54124ab02
iteba15 <allaniteba37@gmail.com>
2023-04-05 17:36:12 -0700

    correction for 1st commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,7 +2,6 @@
 
 /**
  * wildcmp - function that compares two strings
- * wildcmp - function that compares two strings
  * @s1: string 1
  * @s2: string 2 it cotains a * as a special character
  * Return: 1 if they are same, 0 it they are not
@@ -13,19 +12,10 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
 	if (*s1 == '\0' && *s2 == '\0')
-	{
 		return (1);
-	}
-	if (*s1 == *s2 || *s2 == '*')
-	{
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
 	if (*s2 == '*')
-	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
